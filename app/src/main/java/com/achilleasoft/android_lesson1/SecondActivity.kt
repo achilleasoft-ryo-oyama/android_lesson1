123df9b7feddf67febc14f9fea529528bc8c0139
miknam <m.nambu@wakka.co>
2023-01-29 10:01:30 +0900

    追加です

--- app/src/main/java/com/achilleasoft/android_lesson1/SecondActivity.kt
+++ app/src/main/java/com/achilleasoft/android_lesson1/SecondActivity.kt
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.coroutines.NonCancellable.message
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         val intent: Intent = getIntent()
-        val message: String? = intent.getStringExtra(MainActivity().text)
+        val message: String? = intent.getStringExtra(MainActivity()."text")
         val textView: TextView = findViewById(R.id.textView)
         textView.setText(message)
         }
